230e1a7127b4c8286d3f0a31d6d3fc6ed5223433
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-09-18 20:10:21 -0300

    adding 6 examples to example app

--- example/App.js
+++ example/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, Picker } from 'react-native';
 import Markdown, {
-  AstRenderer,
-  styles,
-  renderRules,
-  getUniqueID,
+	AstRenderer,
+	styles,
+	renderRules,
+	getUniqueID, PluginContainer,
 } from 'react-native-markdown-renderer';
-import copyAll from "./src/copyAll";
-import customMarkdownStyle from "./src/customMarkdownStyle";
+import markdownItCheckbox from 'markdown-it-checkbox';
+import copyAll from './src/copyAll';
+import customMarkdownStyle from './src/customMarkdownStyle';
+import copyAllCheckboxPlugin from "./src/copyAllCheckboxPlugin";
+import pluginRules from "./src/pluginRules";
+
+const rules = {
+  h1: (node, children, parents) => {
+    return <Text key={getUniqueID()} style={{ backgroundColor: 'red' }}>{children}</Text>;
+  },
+  // added custom block element defined by plugin
+  block: (node, children, parents) => {
+    return <Text key={getUniqueID()} style={{ backgroundColor: 'green' }}>{children}</Text>;
+  },
+};
 
 /**
  * i'm overriding the default h1 render function.
@@ -15,13 +28,7 @@ import customMarkdownStyle from "./src/customMarkdownStyle";
 const renderer = new AstRenderer(
   {
     ...renderRules,
-    h1: (node, children, parents) => {
-      return <Text key={getUniqueID()} style={{ backgroundColor: 'red' }}>{children}</Text>;
-    },
-    // added custom block element defined by plugin
-    block: (node, children, parents) => {
-      return <Text key={getUniqueID()} style={{ backgroundColor: 'green' }}>{children}</Text>;
-    },
+    ...rules,
   },
   styles
 );
@@ -32,15 +39,17 @@ export default class App extends Component {
   };
 
   list = [
-      { description: 'default' },
-      { description: 'custom renderer' },
-      { description: 'custom style sheet' }
-      ];
+    { description: 'default' },
+    { description: 'custom renderer' },
+    { description: 'custom style sheet' },
+    { description: 'custom rules' },
+    { description: 'custom rules & styles' },
+    { description: 'plugins (checkbox)' },
+  ];
 
   getView(value) {
-
     switch (value) {
-      case 0: {
+      case 6: {
         return <Markdown children={copyAll} />;
       }
       case 1: {
@@ -49,25 +58,31 @@ export default class App extends Component {
       case 2: {
         return <Markdown style={customMarkdownStyle} children={copyAll} />;
       }
+      case 3: {
+        return <Markdown rules={rules} children={copyAll} />;
+      }
+      case 4: {
+        return <Markdown rules={rules} style={customMarkdownStyle} children={copyAll} />;
+      }
+      case 0: {
+        return <Markdown rules={pluginRules} plugins={[new PluginContainer(markdownItCheckbox, {divWrap: true})]} style={customMarkdownStyle} children={copyAllCheckboxPlugin} />;
+      }
       default: {
-        return (
-	        <Markdown># Text</Markdown>
-        );
+        return <Markdown># Text</Markdown>;
       }
     }
   }
 
   render() {
-
-
-
     return (
       <View style={styleSheet.container}>
         <Picker
           selectedValue={this.state.view}
           onValueChange={(itemValue, itemIndex) => this.setState({ view: itemIndex })}
         >
-          {this.list.map((val, index) => <Picker.Item key={val.description} label={val.description} value={index} />)}
+          {this.list.map((val, index) =>
+            <Picker.Item key={val.description} label={val.description} value={index} />
+          )}
         </Picker>
 
         <ScrollView>
