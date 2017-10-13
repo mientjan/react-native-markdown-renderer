867f81a48d81ad4f4ab3fe21c0ed8cdd98903cb1
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-10-13 18:12:33 -0300

    adding blockquote styling to default stylesheet

--- example/src/copyAll.js
+++ example/src/copyAll.js
@@ -59,7 +59,9 @@ _This is italic text_
 
 
 > Blockquotes can also be nested...
+
 >> ...by using additional greater-than signs right next to each other...
+
 > > > ...or with spaces between arrows.
 
 
