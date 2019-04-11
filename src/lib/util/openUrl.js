a2f04e1c0dc90e29fddd1a41731a72fb005941c3
Kyle Clegg <kyle.clegg@lifeomic.com>
2019-04-10 21:56:28 -0600

    Add in-app browser viewing for links

--- src/lib/util/openUrl.js
+++ src/lib/util/openUrl.js
@@ -1,7 +1,17 @@
-import { Linking } from 'react-native';
+import { Platform } from 'react-native';
+import SafariView from 'react-native-safari-view';
+import { ANIMATIONS_SLIDE, CustomTabs } from 'react-native-custom-tabs';
 
 export default function openUrl(url) {
   if (url) {
-    Linking.openURL(url);
+    if (Platform.OS === 'ios') {
+      SafariView.show({
+        url: url,
+      });
+    } else {
+      CustomTabs.openURL(url, {
+        animations: ANIMATIONS_SLIDE,
+      });
+    }
   }
 }
