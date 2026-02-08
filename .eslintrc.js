2a44e4ca2931f0b3f804ebabb0a5446412ca3101
Mient-jan Stelling <mientjan.stelling@gmail.com>
2026-02-08 22:20:21 +0100

    chore: set up build tooling and project infrastructure for v4
    - Add TypeScript, ESLint, Jest, Babel, and react-native-builder-bob configs
    - Update package.json with modern deps (markdown-it ^14, React 18+, RN 0.73+)
    - Remove prop-types, react-native-fit-image, chokidar, fs-extra
    - Replace .npmignore with files field in package.json
    - Add CJS + ESM + TypeScript build targets via builder-bob

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['lib/', 'example/', 'node_modules/'],
+};
